0ceb72ec25981e0c65a89419808cd08a1af6832b
Adsfk <alv_morenitu@hotmail.com>
2019-12-17 09:33:11 +0000

    Refactor - Test renamed

--- src/test/java/PowerCompanyTest.java
+++ src/test/java/PowerCompanyTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PowerCompanyTest {
 
     @Test
-    public void should_return_all_right() {
+    public void should_return_all_right_with_no_bill() {
         BillChecker checker = new BillChecker();
         assertThat(checker.checkBills()).isEqualTo("All right");
     }
